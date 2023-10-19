b6b343b46bdd26caf6e64cb027abb8b156767c24
Agustin Morro <agustin.morro@gmail.com>
2023-10-19 20:51:19 -0300

    second-cmmt

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/diegovargas" element={<PlayerInfo />} />
-          <Route path="/carousel" element={<Carousel />} />
         </Routes>
       </Router>
     </>
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,29 +1,41 @@
 import imgSuela from "/Logo SP negro.svg";
-
+import { Link } from "react-router-dom";
 
 const SideBar = () => {
-    return (
-      <section className="sidebar"  >
-        <div className="side-img_suela">
-          <img src={imgSuela} alt="" />
-       </div>
-          <nav className="side-wrapper">
-            <div className="side-btns">
-              <ul>
-                <li>
-                  <i className="bx bx-male"></i>
-                </li>
-                <li>
-                  <i className="bx bx-trophy"></i>
-                </li>
-                <li>
-                  <i className="bx bxs-contact"></i>
-                </li>
-              </ul>
-            </div>
-          </nav>
-      </section>
-    );
-  };
-  
-  export default SideBar;
+  return (
+    <section className="sidebar">
+      <div className="side-img_suela">
+        <img src={imgSuela} alt="" />
+      </div>
+      <nav className="side-wrapper">
+        <div className="side-btns">
+          <ul>
+            <li>
+              <Link
+                style={{ color: "rgba(172, 114, 8, 0.842)" }}
+                to={"/diegovargas"}
+              >
+                <i className="bx bx-male"></i>
+              </Link>
+            </li>
+            <li>
+              <Link
+                style={{ color: "rgba(172, 114, 8, 0.842)" }}
+                to={"/sobrenosotros"}
+              >
+                <i className="bx bxs-contact"></i>
+              </Link>
+            </li>
+            <li>
+              <Link style={{ color: "rgba(172, 114, 8, 0.842)" }} to={"/"}>
+                <i class="bx bx-home"></i>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </section>
+  );
+};
+
+export default SideBar;
